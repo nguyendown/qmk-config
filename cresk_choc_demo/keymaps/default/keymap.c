31fcfcef2174bd92cc415e62ca28d8a87c9dd931
down <nguyendown@gmail.com>
2022-12-23 00:51:27 +0700

    cresk_choc_demo: update default keymap

--- cresk_choc_demo/keymaps/default/keymap.c
+++ cresk_choc_demo/keymaps/default/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
       * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ' │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
@@ -15,9 +15,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┘   └───┘
       */
     [0] = LAYOUT_split_3x5_2(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                                            KC_BSPC, KC_TAB,           KC_SPC,  KC_ENT
-    )
+        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_QUOTE,
+        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,
+        OSL(1), KC_SPACE, OSM(MOD_LSFT), OSM(MOD_LCTL)
+        ),
+    [1] = LAYOUT_split_3x5_2(
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
+        KC_ESCAPE, KC_SEMICOLON, KC_DOWN, KC_BACKSPACE, KC_INSERT, KC_UP, KC_DELETE, KC_END, KC_HOME, KC_TAB,
+        KC_GRAVE, KC_PAGE_UP, KC_LEFT_BRACKET, KC_ENTER, KC_LEFT, KC_RIGHT, KC_RIGHT_BRACKET, KC_MINUS, KC_EQUAL, KC_BACKSLASH,
+        OSL(2), KC_TRNS, OSM(MOD_LALT), OSM(MOD_LGUI)
+        ),
+    [2] = LAYOUT_split_3x5_2(
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+        KC_F11, KC_F12, KC_TRNS, KC_BACKSPACE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_PAGE_DOWN, KC_TRNS, KC_ENTER, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_NO, KC_TRNS, OSM(MOD_LSFT | MOD_LALT), OSM(MOD_LCTL | MOD_LGUI)
+        ),
 };
